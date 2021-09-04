bcc0706da0a69f66486ae5cdeebb2bd6b536756d
rars9le <le9240@gmail.com>
2021-09-04 20:27:22 +0900

    return message

--- discordbot.js
+++ discordbot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', msg => {
+client.on('message', msg => {
   if (msg.content === '/ping') {
     msg.reply('pong');
   }
